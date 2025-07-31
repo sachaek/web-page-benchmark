4a31c36fd8cc1f6703de3d6ffa634b59dc1c4d12
sacha_ek <sacha-ek@yandex.ru>
2025-07-31 16:29:29 +0500

    базовый PageDriverOptions

--- run.py
+++ run.py
@@ -2,3 +2,20 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.get("https://ya.ru/")
+
+class PageDriver:
+    def __init__(self):
+        self.driver = self._setup_driver()
+
+    def _setup_driver(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        return webdriver.Chrome(options=options)
+
+    def close(self):
+        self.driver.quit()
+
+
+page = PageDriver()._setup_driver()
+page.get("https://ya.ru/")
+page.pause()
